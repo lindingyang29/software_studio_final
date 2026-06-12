9912f057dd7362e8e7b02e0d6a1655b623bef07c
lindingyang29 <lindingyang29@gmail.com>
2026-06-13 05:57:53 +0800

    deploy: background story panel

---
+++ src/settings.be0c8.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Start.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"8cebf",main:"82ec2"}};
